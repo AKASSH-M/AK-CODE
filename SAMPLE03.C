a46ca2842f82b1c7d80e61e8e14005314df0c59c
AKASSH-M <166836871+AKASSH-M@users.noreply.github.com>
2024-04-12 20:09:25 +0530

    Add files via upload

---
+++ SAMPLE03.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ int number = 5000;
+ float pi = 3.14;
+ char letter = 'N';
+ clrscr();
+ printf("The value of number is %d\n",number);
+ printf("The value of pi  is %f\n",pi);
+ printf("The letter is %c",letter);
+ getch();
+ }
